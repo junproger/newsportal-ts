5a98e7eb92f73cb71d68303f43b6d6b450847e45
Junior Programmer <progphyton@gmail.com>
2023-06-09 20:07:30 +0300

    init: start migration-to-typescript task

---
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -0,0 +1,11 @@
+import Loader from './loader';
+
+class AppLoader extends Loader {
+    constructor() {
+        super('http://127.0.0.1:8075/', {
+            apiKey: 'be32012c2d38403794144aeaf00a8297', // получите свой ключ https://newsapi.org/
+        });
+    }
+}
+
+export default AppLoader;
