ac9d2f7202b9a1d45370b77789d0d7766c60b92c
Junior Programmer <progphyton@gmail.com>
2023-06-12 15:46:46 +0300

    refactor: webpack configs

--- migration-to-ts/webpack.dev.config.js
+++ migration-to-ts/webpack.dev.config.js
@@ -5,7 +5,7 @@ module.exports = {
     devtool: 'inline-source-map',
     devServer: {
         static: {
-            directory: path.join(__dirname, 'public'),
+            directory: path.join(__dirname, '../dist'),
         },
         compress: true,
         port: 9000,
