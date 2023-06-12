ef77d83f3b61b82fab7e14ab8aa10701865b74c1
Junior Programmer <progphyton@gmail.com>
2023-06-12 21:28:35 +0300

    feat: add favicon

--- migration-to-ts/src/index.ts
+++ migration-to-ts/src/index.ts
@@ -2,6 +2,7 @@ import App from './components/app/app';
 import { typeApp } from './components/app/types/typeApp';
 
 import './global.css';
+import './assets/favicon.ico';
 
 const app: typeApp = new App();
 app.start();
